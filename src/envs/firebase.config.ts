c9fb78ca9e20e5ce023f1ef76137756347710b9d
Orlando Garcia <orlando.garcia.64@hotmail.com>
2022-10-11 00:08:18 -0500

    for now

---
+++ src/envs/firebase.config.ts
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyBKD3RYeKL-OlOX2wsV0To8FbRX0MyaOy8",
+    authDomain: "wedy-tickets.firebaseapp.com",
+    projectId: "wedy-tickets",
+    storageBucket: "wedy-tickets.appspot.com",
+    messagingSenderId: "748063404312",
+    appId: "1:748063404312:web:2e10001d4c696f843bae6d"
+};
+
---
+++ src/providers/firebase.ts
@@ -0,0 +1,5 @@
+
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from './../envs/firebase.config';
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
